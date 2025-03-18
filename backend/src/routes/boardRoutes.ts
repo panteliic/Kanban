8ec497278cacf9a69d745bb8dd76c0d90bda9702
Nikola Pantelic <pantelicnikolaa@gmail.com>
2025-03-18 18:14:39 +0100

    created route "create new board"

--- backend/src/routes/boardRoutes.ts
+++ backend/src/routes/boardRoutes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { CreateBoard } from "../controllers/board/createBoard";
 
 const passport = require("passport");
 
 const router = Router();
 
+router.post("/create", CreateBoard)
 export default router;
