4df05a5dd2ba603ee72b358c50b52be4899fe7a9
Nikola Pantelic <pantelicnikolaa@gmail.com>
2025-03-18 18:30:41 +0100

    get all user boards

--- backend/src/routes/boardRoutes.ts
+++ backend/src/routes/boardRoutes.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
 import { CreateBoard } from "../controllers/board/createBoard";
-
-const passport = require("passport");
-
+import { getUserBoards } from "../controllers/board/getBoards";
 const router = Router();
 
 router.post("/create", CreateBoard)
+router.get("/getBoards/:userId", getUserBoards)
 export default router;
